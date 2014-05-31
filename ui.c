bd4a5fe7aa495c1b1ceed6d6308662888e162b54
BenoitSchillings <benoit.schillings@gmail.com>
2014-05-31 12:19:07 -0700

    added double buffer for nvidia cards

--- ui.c
+++ ui.c
@@ -1,9 +1,3 @@
-// very quick and dirty UI for bladerf waterfall rendering.
-// feel free to cleanup, improve, remove global variables, etc...
-// Benoit Schillings, May 2013
-
-
-
 
 #include <GL/glut.h>
 #include <unistd.h>     // Header file for sleeping.
@@ -208,6 +202,8 @@ void ddraw()
 void display()
 {
     ddraw();
+    glFlush(); 
+    glutSwapBuffers(); 
     return;
     
     glClear(GL_COLOR_BUFFER_BIT);
@@ -424,13 +420,28 @@ void grid_init() {
     glClearColor(0.1, 0.1, 0.1, 1.0);
     glColor3f(1.0, 1.0, 1.0);
     
+    // Tell the rendering engine not to draw backfaces.  Without this code,
+    // all four faces of the tetrahedron would be drawn and it is possible
+    // that faces farther away could be drawn after nearer to the viewer.
+    // Since there is only one closed polyhedron in the whole scene,
+    // eliminating the drawing of backfaces gives us the realism we need.
+    // THIS DOES NOT WORK IN GENERAL.
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     
+    // Set the camera lens so that we have a perspective viewing volume whose
+    // horizontal bounds at the near clipping plane are -2..2 and vertical
+    // bounds are -1.5..1.5.  The near clipping plane is 1 unit from the camera
+    // and the far clipping plane is 40 units away.
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glFrustum(-2, 2, -1.5, 1.5, 1, 340);
     glEnable(GL_BLEND);
+    // Set up transforms so that the tetrahedron which is defined right at
+    // the origin will be rotated and moved into the view volume.  First we
+    // rotate 70 degrees around y so we can see a lot of the left side.
+    // Then we rotate 50 degrees around x to "drop" the top of the pyramid
+    // down a bit.  Then we move the object back 3 units "into the screen".
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glTranslatef(0, 0, -5);
@@ -444,7 +455,7 @@ void grid_init() {
 int main(int argc, char ** argv)
 {
     init_fft(argc, argv);
-   
+  
     for (int i = 0; i < 256; i++) {
         colors[i * 3 + 0] = i;
         colors[i * 3 + 1] = i;
@@ -456,7 +467,7 @@ int main(int argc, char ** argv)
     }
     
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     
     glutInitWindowSize(WIDTH, 800);
     glutInitWindowPosition(50, 50);
